406e0acd7da93bdd02dec4558de92249728f46f1
mihaicf <fuchedup@yahoo.com>
2020-06-25 21:24:54 +0300

    Updates

---
+++ precache-manifest.154cb946555d8a4375bd716d24f9948c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4ecbdac6718f719e35565aba1437a1d",
+    "url": "/robodex/index.html"
+  },
+  {
+    "revision": "49bd7135f4b984b9331b",
+    "url": "/robodex/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6946bf59feae719b4c92",
+    "url": "/robodex/static/css/main.549b5d7b.chunk.css"
+  },
+  {
+    "revision": "49bd7135f4b984b9331b",
+    "url": "/robodex/static/js/2.a7721c1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robodex/static/js/2.a7721c1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6946bf59feae719b4c92",
+    "url": "/robodex/static/js/main.c68db5ef.chunk.js"
+  },
+  {
+    "revision": "07594d1dabbd772789a3",
+    "url": "/robodex/static/js/runtime-main.b674caba.js"
+  }
+]);
